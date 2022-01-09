a5514e10414487d2bb71a95db4aed163668abb9e
ilya <ilyaskvortsov1806@gmail.com>
2022-01-09 20:48:38 +0300

    добавил 5 задание

---
+++ homework№5/5.py
@@ -0,0 +1,15 @@
+# задание №5
+set_1 =(1 ,2)
+set_2 =(3,4,5,6,7)
+set_3 =(8,9,10)
+
+print(set_1)
+print(set_2)
+print(set_3)
+
+# дополнитеьное задание
+string_1 = list(input("Enter a string: "))
+print(string_1)
+x = string_1.pop(5)
+print(x)
+print(string_1)
